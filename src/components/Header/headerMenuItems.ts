363c71c323d9c4bbc42e4c8a68dcfbb456c13a18
tomo <09bkhair19@gmail.com>
2026-03-01 21:40:24 +0900

    feat: 現状blogは公開しない

--- src/components/Header/headerMenuItems.ts
+++ src/components/Header/headerMenuItems.ts
@@ -10,6 +10,5 @@ export const HEADER_MENU_ITEMS = [
   { label: "活動内容", href: "/activities" },
   { label: "作品紹介", href: "/works" },
   { label: "お知らせ", href: "/news" },
-  { label: "blog", href: "/blog" },
   { label: "お問い合わせ", href: "/contact" },
 ] satisfies HeaderMenuItem[];
