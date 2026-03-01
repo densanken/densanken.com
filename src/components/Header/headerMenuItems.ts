dbebdb05b96281a745a60298e760707aa23517fd
tomo <09bkhair19@gmail.com>
2026-03-01 18:09:25 +0900

    refac: satisfiesの使用

--- src/components/Header/headerMenuItems.ts
+++ src/components/Header/headerMenuItems.ts
@@ -5,11 +5,11 @@ export type HeaderMenuItem = {
 
 export const HEADER_TITLE = "CCS";
 
-export const HEADER_MENU_ITEMS: HeaderMenuItem[] = [
+export const HEADER_MENU_ITEMS = [
   { label: "ホーム", href: "/" },
   { label: "活動内容", href: "/activities" },
   { label: "作品紹介", href: "/works" },
   { label: "お知らせ", href: "/news" },
   { label: "blog", href: "/blog" },
   { label: "お問い合わせ", href: "/contact" },
-];
+] satisfies HeaderMenuItem[];
