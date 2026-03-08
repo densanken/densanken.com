7f98a28a0d11d584f5d4bd2ba2a6a0aed1e06724
ryosanhin <90621212+ryosanhin@users.noreply.github.com>
2026-03-08 07:21:42 +0000

    feat: worksディレクトリ以下を追加

---
+++ src/pages/works/games.ts
@@ -0,0 +1,28 @@
+export interface GameData {
+  title: string;
+  description: string;
+  imageSrc?: string;
+  detailHref?: string;
+}
+
+export const games: GameData[] = [
+  {
+    title: "今日は大漁",
+    description: "ここは海。今日の釣果はあなた次第。探知機を使って魚群を見つけてすべて捕まえよう！",
+    imageSrc: "/src/assets/sanma.png",
+    detailHref: "#",
+  },
+  {
+    title: "今日は大漁",
+    description: "ここは海。今日の釣果はあなた次第。探知機を使って魚群を見つけてすべて捕まえよう！",
+  },
+  {
+    title: "今日は大漁",
+    description: "ここは海。今日の釣果はあなた次第。探知機を使って魚群を見つけてすべて捕まえよう！",
+    imageSrc: "/src/assets/sanma.png",
+  },
+  {
+    title: "今日は大漁",
+    description: "ここは海。今日の釣果はあなた次第。探知機を使って魚群を見つけてすべて捕まえよう！",
+  },
+];
