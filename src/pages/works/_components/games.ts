14e9efbf133884b1c82821de82bd889b52d613d2
ryosanhin <90621212+ryosanhin@users.noreply.github.com>
2026-03-10 03:35:34 +0000

    feat: paginationを実装

--- src/pages/works/_components/games.ts
+++ src/pages/works/_components/games.ts
@@ -1,5 +1,7 @@
 export interface GameData {
   title: string;
+  id: string;
+  year: string;
   description: string;
   imageSrc?: string;
   detailHref?: string;
@@ -8,21 +10,77 @@ export interface GameData {
 export const games: GameData[] = [
   {
     title: "今日は大漁",
+    id: "test_0",
+    year: "2000",
     description: "ここは海。今日の釣果はあなた次第。探知機を使って魚群を見つけてすべて捕まえよう！",
     imageSrc: "sanma.png",
     detailHref: "#",
   },
   {
     title: "今日は大漁",
+    id: "test_1",
+    year: "2000",
     description: "ここは海。今日の釣果はあなた次第。探知機を使って魚群を見つけてすべて捕まえよう！",
   },
   {
     title: "今日は大漁",
+    id: "test_2",
+    year: "2000",
     description: "ここは海。今日の釣果はあなた次第。探知機を使って魚群を見つけてすべて捕まえよう！",
     imageSrc: "sanma.png",
   },
   {
     title: "今日は大漁",
+    id: "test_3",
+    year: "2000",
+    description: "ここは海。今日の釣果はあなた次第。探知機を使って魚群を見つけてすべて捕まえよう！",
+  },
+  {
+    title: "今日は大漁",
+    id: "test_4",
+    year: "2000",
+    description: "ここは海。今日の釣果はあなた次第。探知機を使って魚群を見つけてすべて捕まえよう！",
+  },
+  {
+    title: "今日は大漁",
+    id: "test_5",
+    year: "2000",
+    description: "ここは海。今日の釣果はあなた次第。探知機を使って魚群を見つけてすべて捕まえよう！",
+  },
+  {
+    title: "今日は大漁",
+    id: "test_6",
+    year: "2000",
+    description: "ここは海。今日の釣果はあなた次第。探知機を使って魚群を見つけてすべて捕まえよう！",
+  },
+  {
+    title: "今日は大漁",
+    id: "test_7",
+    year: "2000",
+    description: "ここは海。今日の釣果はあなた次第。探知機を使って魚群を見つけてすべて捕まえよう！",
+  },
+  {
+    title: "今日は大漁",
+    id: "test_8",
+    year: "2000",
+    description: "ここは海。今日の釣果はあなた次第。探知機を使って魚群を見つけてすべて捕まえよう！",
+  },
+  {
+    title: "今日は大漁",
+    id: "test_9",
+    year: "2000",
+    description: "ここは海。今日の釣果はあなた次第。探知機を使って魚群を見つけてすべて捕まえよう！",
+  },
+  {
+    title: "今日は大漁",
+    id: "test_10",
+    year: "2000",
+    description: "ここは海。今日の釣果はあなた次第。探知機を使って魚群を見つけてすべて捕まえよう！",
+  },
+  {
+    title: "今日は大漁",
+    id: "test_11",
+    year: "2000",
     description: "ここは海。今日の釣果はあなた次第。探知機を使って魚群を見つけてすべて捕まえよう！",
   },
 ];
