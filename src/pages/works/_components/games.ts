f3012a065b22b270ccea6f81cccb4ccad1b822ea
ryosanhin <90621212+ryosanhin@users.noreply.github.com>
2026-03-13 03:41:24 +0000

    feat: ページ下部のページごとに表示の変わるボタンの実装だけ実行

--- src/pages/works/_components/games.ts
+++ src/pages/works/_components/games.ts
@@ -1,86 +1,20 @@
 export interface GameData {
   title: string;
   id: string;
-  year: string;
+  year: number | null;
   description: string;
   imageSrc?: string;
-  detailHref?: string;
+  atwikiUrl?: string;
 }
 
-export const games: GameData[] = [
-  {
-    title: "今日は大漁",
-    id: "test_0",
-    year: "2000",
-    description: "ここは海。今日の釣果はあなた次第。探知機を使って魚群を見つけてすべて捕まえよう！",
-    imageSrc: "sanma.png",
-    detailHref: "#",
-  },
-  {
-    title: "今日は大漁",
-    id: "test_1",
-    year: "2000",
-    description: "ここは海。今日の釣果はあなた次第。探知機を使って魚群を見つけてすべて捕まえよう！",
-  },
-  {
-    title: "今日は大漁",
-    id: "test_2",
-    year: "2000",
-    description: "ここは海。今日の釣果はあなた次第。探知機を使って魚群を見つけてすべて捕まえよう！",
-    imageSrc: "sanma.png",
-  },
-  {
-    title: "今日は大漁",
-    id: "test_3",
-    year: "2000",
-    description: "ここは海。今日の釣果はあなた次第。探知機を使って魚群を見つけてすべて捕まえよう！",
-  },
-  {
-    title: "今日は大漁",
-    id: "test_4",
-    year: "2000",
-    description: "ここは海。今日の釣果はあなた次第。探知機を使って魚群を見つけてすべて捕まえよう！",
-  },
-  {
-    title: "今日は大漁",
-    id: "test_5",
-    year: "2000",
-    description: "ここは海。今日の釣果はあなた次第。探知機を使って魚群を見つけてすべて捕まえよう！",
-  },
-  {
-    title: "今日は大漁",
-    id: "test_6",
-    year: "2000",
-    description: "ここは海。今日の釣果はあなた次第。探知機を使って魚群を見つけてすべて捕まえよう！",
-  },
-  {
-    title: "今日は大漁",
-    id: "test_7",
-    year: "2000",
-    description: "ここは海。今日の釣果はあなた次第。探知機を使って魚群を見つけてすべて捕まえよう！",
-  },
-  {
-    title: "今日は大漁",
-    id: "test_8",
-    year: "2000",
-    description: "ここは海。今日の釣果はあなた次第。探知機を使って魚群を見つけてすべて捕まえよう！",
-  },
-  {
-    title: "今日は大漁",
-    id: "test_9",
-    year: "2000",
-    description: "ここは海。今日の釣果はあなた次第。探知機を使って魚群を見つけてすべて捕まえよう！",
-  },
-  {
-    title: "今日は大漁",
-    id: "test_10",
-    year: "2000",
-    description: "ここは海。今日の釣果はあなた次第。探知機を使って魚群を見つけてすべて捕まえよう！",
-  },
-  {
-    title: "今日は大漁",
-    id: "test_11",
-    year: "2000",
-    description: "ここは海。今日の釣果はあなた次第。探知機を使って魚群を見つけてすべて捕まえよう！",
-  },
-];
+export const games = Array.from({ length: 300 }, (_, i) => {
+  const num = i + 1;
+
+  return {
+    title: `テスト作品 ${num.toString()}`,
+    id: num.toString(),
+    year: 2000 + (num % 20),
+    description: `テスト作品 ${num.toString()}です。`,
+    atwikiUrl: "#",
+  };
+}) satisfies GameData[];
