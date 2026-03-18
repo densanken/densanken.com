6488d582b1cd141568ff2a7fcc3abf0ab3571f6a
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2026-03-18 22:41:45 +0900

    chore: declare contact and join form URL env vars

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly CONTACT_GOOGLE_FORM_URL: string;
+  readonly JOIN_GOOGLE_FORM_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
