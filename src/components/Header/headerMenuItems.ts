740d093651e72d7b58caa3dd481d8d83bf4d17dd
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2026-03-24 00:29:59 +0900

    chore: hide news and works links and sections

--- src/components/Header/headerMenuItems.ts
+++ src/components/Header/headerMenuItems.ts
@@ -8,7 +8,7 @@ export const HEADER_TITLE = "CCS";
 export const HEADER_MENU_ITEMS = [
   { label: "ホーム", href: "/" },
   { label: "活動内容", href: "/activities" },
-  { label: "作品紹介", href: "/works" },
-  { label: "お知らせ", href: "/news" },
+  // { label: "作品一覧", href: "/works" },
+  // { label: "お知らせ", href: "/news" },
   { label: "お問い合わせ", href: "/contact" },
 ] satisfies HeaderMenuItem[];
