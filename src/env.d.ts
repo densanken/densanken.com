7ca71b8adc56b55c774417798ac717b2e49e1aea
Yusuke <67366917+Yusuke4869@users.noreply.github.com>
2026-03-24 11:14:43 +0900

    feat: add google analytics tracking (#70)

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
+  readonly GOOGLE_ANALYTICS_ID?: string;
   readonly CONTACT_GOOGLE_FORM_URL: string;
   readonly JOIN_GOOGLE_FORM_URL: string;
 }
